5c1eab3106b384ee9d2f31d403901458f34ba8d1
Krishna Patel <kcpn14@gmail.com>
2023-06-04 20:15:49 -0400

    added edit, delete module in profile

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,55 @@
-import React from 'react'
+'use client';
+
+import React, { useEffect, useState } from 'react'
+import PromptCard from './PromptCard';
+
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className='mt-16 prompt_layout'>
+      {data.map((post) => (
+        <PromptCard
+        key={post._id}
+        post={post}
+        handleTagClick={handleTagClick}
+         />
+      ))}
+    </div>
+  )
+}
 
 const Feed = () => {
+
+  const [searchText, setSearchText] = useState('');
+  const [post, setPost] = useState([])
+
+  const handleSearchChange = (e) => {
+    e.preventDefault();
+  }
+
+useEffect(()=>{
+  const fetchPost = async () => {
+    const response = await fetch('api/prompt');
+    const data = await response.json();
+    setPost(data);
+  }
+
+  fetchPost();
+},[])
+
   return (
-    <div>Feed</div>
+    <section className='feed'>
+      <form className='relative w-full flex-center'>
+        <input type="text"
+          placeholder='Search for tag or a username'
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className='search_input peer'
+        />
+      </form>
+
+      <PromptCardList data={post} handleTagClick={() => { }} />
+    </section>
   )
 }
 
